fa8324ff2b089bfc107982d5ffa0557642327ee6
grisov <grisov.gmail@mailnull.com>
2018-05-22 23:49:16 +0300

    Added unittest.TestCase class

---
+++ test/test_output.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+import unittest
+
+
+class OutputTests(unittest.TestCase):
+    """x"""
+
+    def test_urls_404(self):
+        """x"""
+        self.assertEqual(1+1, 2)
+
+    def test_urls_50x(self):
+        """x"""
+        self.assertEqual(1+1, 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
