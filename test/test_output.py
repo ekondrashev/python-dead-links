db462ffc7500a798295c5dd6ba4644fa7df451c3
grisov <grisov.gmail@mailnull.com>
2018-05-23 23:50:59 +0300

    Added comments to the tests

--- test/test_output.py
+++ test/test_output.py
@@ -6,44 +6,46 @@ from subprocess import Popen, PIPE, STDOUT
 
 
 class OutputTests(unittest.TestCase):
-    """x"""
+    """Testing the correctness of the script output data"""
 
     @classmethod
     def setUpClass(cls):
+        """Running the script and retrieving data"""
         cls._url = 'http://massage.zt.ua'
         p = Popen(['python', 'main.py', cls._url], stderr=STDOUT, stdout=PIPE, shell=True)
         cls._stdout = p.stdout.read()
 
     @classmethod
     def tearDownClass(cls):
+        """Resetting data"""
         cls._stdout = ''
 
     def test_url_to_check(self):
-        """x"""
+        """Verification of the tested url-address"""
         actual = json.loads(self._stdout).get('url')
         self.assertEqual(actual, self._url)
 
     def test_urls_404(self):
-        """x"""
+        """Checking URLs with status 404"""
         actual = json.loads(self._stdout).get('404')
         self.assertEqual(actual.get('size'), 0)
         self.assertEqual(actual.get('urls'), [])
         self.assertEqual(len(actual.get('urls')), actual.get('size'))
 
     def test_urls_50x(self):
-        """x"""
+        """Checking URLs with status 50x"""
         actual = json.loads(self._stdout).get('50x')
         self.assertEqual(actual.get('size'), 0)
         self.assertEqual(actual.get('urls'), [])
         self.assertEqual(len(actual.get('urls')), actual.get('size'))
 
     def test_total_dead_urls(self):
-        """x"""
+        """Checking the total number of broken URLs"""
         actual = json.loads(self._stdout).get('dead')
         self.assertEqual(actual, 0)
 
     def test_total_urls(self):
-        """x"""
+        """Checking the total number of URLs found on the page"""
         actual = json.loads(self._stdout).get('total')
         self.assertEqual(actual, 17)
 
